6fed1e378ec3aa0b9bdc557033f09db071a015df
darklink623 <darklink623@gmail.com>
2018-11-29 22:31:48 -0500

    Add files via upload

---
+++ n64TexConverterCI/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace n64TexConverterCI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.filePathBtn = new System.Windows.Forms.Button();
+            this.convertBtn = new System.Windows.Forms.Button();
+            this.paletteSizeCmb = new System.Windows.Forms.ComboBox();
+            this.texturePic = new System.Windows.Forms.PictureBox();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.filePathTxt = new System.Windows.Forms.TextBox();
+            this.paletteSizeLbl = new System.Windows.Forms.Label();
+            this.metadataLbl = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.texturePic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // filePathBtn
+            // 
+            this.filePathBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.filePathBtn.Location = new System.Drawing.Point(197, 13);
+            this.filePathBtn.Name = "filePathBtn";
+            this.filePathBtn.Size = new System.Drawing.Size(75, 23);
+            this.filePathBtn.TabIndex = 0;
+            this.filePathBtn.Text = "File Path";
+            this.filePathBtn.UseVisualStyleBackColor = true;
+            this.filePathBtn.Click += new System.EventHandler(this.filePathBtn_Click);
+            // 
+            // convertBtn
+            // 
+            this.convertBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.convertBtn.Location = new System.Drawing.Point(197, 197);
+            this.convertBtn.Name = "convertBtn";
+            this.convertBtn.Size = new System.Drawing.Size(75, 23);
+            this.convertBtn.TabIndex = 1;
+            this.convertBtn.Text = "Convert";
+            this.convertBtn.UseVisualStyleBackColor = true;
+            this.convertBtn.Click += new System.EventHandler(this.convertBtn_Click);
+            // 
+            // paletteSizeCmb
+            // 
+            this.paletteSizeCmb.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.paletteSizeCmb.FormattingEnabled = true;
+            this.paletteSizeCmb.Items.AddRange(new object[] {
+            "16",
+            "256"});
+            this.paletteSizeCmb.Location = new System.Drawing.Point(197, 57);
+            this.paletteSizeCmb.Name = "paletteSizeCmb";
+            this.paletteSizeCmb.Size = new System.Drawing.Size(75, 21);
+            this.paletteSizeCmb.TabIndex = 2;
+            this.paletteSizeCmb.Text = "16";
+            // 
+            // texturePic
+            // 
+            this.texturePic.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.texturePic.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.texturePic.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.texturePic.Location = new System.Drawing.Point(12, 41);
+            this.texturePic.Name = "texturePic";
+            this.texturePic.Size = new System.Drawing.Size(179, 208);
+            this.texturePic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.texturePic.TabIndex = 3;
+            this.texturePic.TabStop = false;
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.exitBtn.Location = new System.Drawing.Point(197, 226);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(75, 23);
+            this.exitBtn.TabIndex = 4;
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // filePathTxt
+            // 
+            this.filePathTxt.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.filePathTxt.Location = new System.Drawing.Point(12, 15);
+            this.filePathTxt.Name = "filePathTxt";
+            this.filePathTxt.ReadOnly = true;
+            this.filePathTxt.Size = new System.Drawing.Size(178, 20);
+            this.filePathTxt.TabIndex = 5;
+            // 
+            // paletteSizeLbl
+            // 
+            this.paletteSizeLbl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.paletteSizeLbl.AutoSize = true;
+            this.paletteSizeLbl.Location = new System.Drawing.Point(197, 41);
+            this.paletteSizeLbl.Name = "paletteSizeLbl";
+            this.paletteSizeLbl.Size = new System.Drawing.Size(66, 13);
+            this.paletteSizeLbl.TabIndex = 6;
+            this.paletteSizeLbl.Text = "Palette Size:";
+            // 
+            // metadataLbl
+            // 
+            this.metadataLbl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.metadataLbl.AutoSize = true;
+            this.metadataLbl.Location = new System.Drawing.Point(197, 85);
+            this.metadataLbl.Name = "metadataLbl";
+            this.metadataLbl.Size = new System.Drawing.Size(51, 13);
+            this.metadataLbl.TabIndex = 7;
+            this.metadataLbl.Text = "metadata";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.metadataLbl);
+            this.Controls.Add(this.paletteSizeLbl);
+            this.Controls.Add(this.filePathTxt);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.texturePic);
+            this.Controls.Add(this.paletteSizeCmb);
+            this.Controls.Add(this.convertBtn);
+            this.Controls.Add(this.filePathBtn);
+            this.MinimumSize = new System.Drawing.Size(210, 210);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CI Texture Converter";
+            ((System.ComponentModel.ISupportInitialize)(this.texturePic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button filePathBtn;
+        private System.Windows.Forms.Button convertBtn;
+        private System.Windows.Forms.ComboBox paletteSizeCmb;
+        private System.Windows.Forms.PictureBox texturePic;
+        private System.Windows.Forms.Button exitBtn;
+        private System.Windows.Forms.TextBox filePathTxt;
+        private System.Windows.Forms.Label paletteSizeLbl;
+		private System.Windows.Forms.Label metadataLbl;
+	}
+}
+
---
+++ n64TexConverterCI/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace n64TexConverterCI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        String fileName;
+        String directory;
+
+        private void convertToCI()
+        {
+            uint[] colors;
+            uint pixelImg;
+
+            if (filePathTxt.Text != null)
+            {
+                // defines
+                Bitmap image = new Bitmap(filePathTxt.Text);
+                int imageX = image.Width;
+                int imageY = image.Height;
+                pixelImg = new uint();
+                
+                StreamWriter imageOutput = new StreamWriter(directory + fileName + ".h");
+                colors = new uint[Convert.ToInt16(paletteSizeCmb.Text)];
+                String pixelHex;
+
+                imageOutput.WriteLine("/*\n * this image was converted using the CI converter\n * this image is {0} pixels wide by {1} pixels tall\n */", imageX, imageY);
+                imageOutput.Write("unsigned char " + fileName.Replace(" ", String.Empty) + "[] = {\n");
+
+                uint r, g, b, a;
+
+                // loop x and y coords and fill picture size with pixel colors
+                for (int j = 0; j < imageY; j++)
+                {
+                    imageOutput.Write("\t");
+                    for (int i = 0; i < imageX; i++)
+                    {
+                        // getting raw data
+                        pixelImg = (uint)image.GetPixel(i, j).ToArgb();
+
+                        // compare with colors array
+                        for (int k = 0; k < colors.Length; k++)
+                        {
+                            if (colors[k] == 0)
+                            {
+                                // add color to array
+                                colors[k] = pixelImg;
+                                pixelImg = (uint)k;
+
+                                // break loop
+                                k = colors.Length;   
+                            }
+                            else
+                            {
+                                // compare colors
+                                if (pixelImg.Equals(colors[k])) 
+                                {
+                                    pixelImg = (uint)k;
+                                    k = colors.Length;
+                                }
+                            }
+                        }
+
+                        // convert to hex
+                        pixelHex = String.Format("0x{0:X}", pixelImg);
+                        if (i != imageX - 1 || j != imageY - 1)
+                        {
+                            pixelHex = pixelHex + ", ";
+                        }
+                        //TODO re enable
+                        //metadataLbl.Text = pixelHex;
+
+                        // write to file
+                        imageOutput.Write(pixelHex);
+                    }
+                    // new line
+                    imageOutput.WriteLine("");
+                }
+                pixelHex = null;
+                //metadataLbl.Text = pixelHex;
+                imageOutput.WriteLine("};");
+
+                // print color palette
+                imageOutput.WriteLine("\nunsigned short " + fileName.Replace(" ", String.Empty) + "Palette[] = {");
+                for (int i = 0; i < colors.Length; i++)
+                {
+                    // set up for bitshifts
+                    r = colors[i];
+                    g = colors[i];
+                    b = colors[i];
+                    a = colors[i];
+
+                    // bitshift
+                    r = r << 8 >> 27 << 11;
+                    g = g << 16 >> 27 << 6;
+                    b = b << 24 >> 27 << 1;
+                    a = a >> 31;
+
+                    //String test = ("0x{0:X}", r);
+
+                    //metadataLbl.Text = ("0x{0:X}", r);
+
+                    colors[i] = r+b+g+a;
+
+                    imageOutput.Write("\t0x{0:X}", colors[i]);
+                    // feels like this could be optimized, its messy
+                    if (i < colors.Length - 1)
+                    {
+                        imageOutput.WriteLine(",");
+
+                    }
+                    else
+                    {
+                        imageOutput.WriteLine();
+                    }
+                    //metadataLbl.Text = ("0x{0:X}", colors[i]);
+                }
+                imageOutput.WriteLine("};");
+
+                // REMEMBER TO CLOSE THE FILE IDIOT
+                imageOutput.Close();
+            }
+            pixelImg = 0;
+            colors = null;
+        }
+
+        private void filePathBtn_Click(object sender, EventArgs e)
+        {
+            FileDialog textureDialog = new OpenFileDialog();
+
+            if (textureDialog.ShowDialog() == DialogResult.OK)
+            {
+                // display image, show url in textbar
+                directory = Path.GetDirectoryName(textureDialog.FileName) + "\\";
+                fileName = Path.GetFileNameWithoutExtension(textureDialog.FileName);
+
+                // removes space
+                fileName.Replace(" ", String.Empty);
+                filePathTxt.Text = textureDialog.FileName;
+                texturePic.Image = Image.FromFile(filePathTxt.Text);
+            }
+        }
+        
+		private void convertBtn_Click(object sender, EventArgs e)
+		{
+            try
+            {
+                convertToCI();
+            }
+            catch (IOException)
+            {
+                MessageBox.Show("Make sure you have an image selected", "ERROR");
+            }
+		}
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            // close application, this should be obvious
+            Close();
+        }
+    }
+}
